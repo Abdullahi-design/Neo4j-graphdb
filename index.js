d2129b215cffcf8e9466068daac6d7ffb840116b
Abdullahi-design <abdullahismohd3000@gmail.com>
2023-07-16 09:38:54 +0100

    we can now create single and multiple connections for a user

--- index.js
+++ index.js
@@ -14,62 +14,56 @@ const url = process.env.CONNECTION_URI;
 const driver = neo4j.driver(`${url}`, neo4j.auth.basic('neo4j', `${password}`));
 
 app.post('/users', (req, res) => {
-  const { 
-    name,
-    fullName,
-    business,
-    location,
-    connectionName,
-    connectionFullName,
-    connectionlastContacted,
-    howDidYouMeet
-   } = req.body;
-
-  // Create a session to interact with the database
-  const session = driver.session();
-
-  // Define your query and parameters
-  const query = `
-  CREATE (person:PERSON {name: $fullName, business: $business, location: $location})
-  WITH person
-  MATCH (connection:PERSON {name: $connectionFullName})
-  CREATE (person)-[:CLIENT {lastContacted: $connectionlastContacted, howDidYouMeet: $howDidYouMeet}]->(connection)
-  RETURN person
-`;
-
-
-  const params = {
-    name,
-    fullName,
-    business,
-    location,
-    connectionName,
-    connectionFullName,
-    connectionlastContacted,
-    howDidYouMeet
-  };
-
-  // Run the query
-  session
-    .run(query, params)
-    .then((result) => {
-      if (result.records.length > 0) {
-        const abdulNode = result.records[0].get('person');
-        console.log('Created node:', abdulNode.properties);
-        res.status(201).json({ message: 'Node created successfully', node: abdulNode.properties });
-      } else {
-        console.log('No records returned from the query.');
-        res.status(500).json({ error: 'An error occurred while creating the node' });
-      }
-    })
-    .catch((error) => {
-      console.error('Error creating node:', error);
-      res.status(500).json({ error: 'An error occurred while creating the node' });
-    })
-    .finally(() => {
-      // Close the session
-      session.close();
-    });
+    const { 
+      name,
+      fullName,
+      business,
+      location,
+      friends
+    } = req.body;
+  
+    // Create a session to interact with the database
+    const session = driver.session();
+  
+    // Define your query and parameters
+    const query = `
+      CREATE (person:PERSON {name: $fullName, business: $business, location: $location})
+      WITH person
+      UNWIND $friends AS friend
+      MATCH (connection:PERSON {name: friend.name})
+      CREATE (person)-[:CLIENT {lastContacted: friend.lastContacted, howDidYouMeet: friend.howDidYouMeet}]->(connection)
+      RETURN person
+    `;
+  
+    const params = {
+      name,
+      fullName,
+      business,
+      location,
+      friends
+    };
+  
+    // Run the query
+    session
+        .run(query, params)
+        .then((result) => {
+            if (result.records.length > 0) {
+            const personNode = result.records[0].get('person');
+            console.log('Created node:', personNode.properties);
+            res.status(201).json({ message: 'Node created successfully', node: personNode.properties });
+            } else {
+            console.log('No records returned from the query.');
+            res.status(500).json({ error: 'An error occurred while creating the node' });
+            }
+        })
+        .catch((error) => {
+            console.error('Error creating node:', error);
+            res.status(500).json({ error: 'An error occurred while creating the node' });
+        })
+        .finally(() => {
+            // Close the session
+            session.close();
+        });
 });
 
 // Start the server
